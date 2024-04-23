062ff65f0541de558c057782b7b2c319e4d96f61
赵信勇 <AyongNice@163.com>
2024-04-23 13:07:05 +0800

    button 优化

--- src/button/demos/disabled.tsx
+++ src/button/demos/disabled.tsx
@@ -1,12 +1,13 @@
 import { Button } from 'ayongui';
 import React from 'react';
 export default () => {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <Button type="text">文字类型</Button>
-            <Button type="primary" disabled>
-                禁用
-            </Button>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <Button type="primary" disabled>
+        禁用
+      </Button>
+
+      <Button type="text">文字类型</Button>
+    </div>
+  );
 };
